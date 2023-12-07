342c7a105e10463ec7d9a5d664b55bd5656a79e4
Paula Prieto <paueprieto@gmail.com>
2023-12-08 00:28:14 +1100

    Update clean_tmp.sh

--- clean_tmp.sh
+++ clean_tmp.sh
@@ -1,1 +1,23 @@
+# Variable with path
+find_tmp=$(/usr/bin/find /tmp -name '*.TMP')
 
+# Variable with current date
+current_date=$(date '+[%a %b%e %T %Y]')
+
+# Check find_tmp is not mpty
+if [ -n "$find_tmp" ]; then
+  # Print current date variable and string
+  printf "$current_date %sDeleted files:" >> /var/log/tmp_cleanup.log 2>&1
+  # Iterates throuh file list
+  for file in $find_tmp; do
+    filename=$(basename $file)
+    # Prints filename to log
+    printf " %s" "$filename" >> /var/log/tmp_cleanup.log 2>&1
+    # Removes tmp file
+    rm "$file"
+  done
+  printf "\n" >> /var/log/tmp_cleanup.log 2>&1
+else
+  # Prints current date variable and string
+  echo "$current_date No temp files found" >> /var/log/tmp_cleanup.log 2>&1
+fi
